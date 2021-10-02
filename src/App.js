5bc51527ae83ae1fb902e2e8d9cafea348ab0a4b
Ganna <annakovalenko168@gmail.com>
2021-10-02 23:19:16 +0300

    стиль для первой задачи

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import user from './components/Task1/user.json';
-import Profile from './components/Task1/Profile';
-import statisticalData from './components/Task2/statistical-data.json';
-import Statistics from './components/Task2/Statistics';
-import friends from './components/Task3/friends.json';
-import FriendList from './components/Task3/FriendList';
-import transactions from './components/Task4/transactions.json';
-import TransactionHistory from './components/Task4/TransactionHistory';
+import user from './components/Profile/user.json';
+import Profile from './components/Profile/Profile';
+import statisticalData from './components/Statistics/statistical-data.json';
+import Statistics from './components/Statistics/Statistics';
+import friends from './components/FriendList/friends.json';
+import FriendList from './components/FriendList/FriendList';
+import transactions from './components/Transactions/transactions.json';
+import TransactionHistory from './components/Transactions/TransactionHistory';
 
 const App = () => {
   return (
@@ -20,11 +20,11 @@ const App = () => {
         views={user.stats.views}
         likes={user.stats.likes}
       />
-      ,
+
       <Statistics title="Upload stats" stats={statisticalData} />
-      <Statistics stats={statisticalData} />,
-      <FriendList friends={friends} />,
-      <TransactionHistory items={transactions} />;
+      <Statistics stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
